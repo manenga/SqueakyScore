49f1ff3902243c579c1641fd83958b618fc68808
Manenga Mungandi <manengam@rain.co.za>
2023-08-11 18:09:15 +0200

    Initial Commit

---
+++ CreditScore/CreditScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditScoreApp.swift
+//  CreditScore
+//
+//  Created by Manenga Mungandi on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct CreditScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
